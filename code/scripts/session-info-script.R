aa65b2e3c5ac2d79dbccdefca2ce8f288d77c997
Lauren Hanlon <laurenmhanlon@gmail.com>
2016-10-26 03:17:06 -0700

    session info

---
+++ code/scripts/session-info-script.R
@@ -0,0 +1,9 @@
+## Library calls to all the packages used in project
+library(ggplot2)
+library(stargazer)
+library(reshape)
+
+## Session Info
+sink("../../session-info.txt")
+sessionInfo()
+sink()
