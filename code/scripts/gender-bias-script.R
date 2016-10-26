e24a1a8b839ae14421b2eb7c7e6acda817cb1963
Lauren Hanlon <laurenmhanlon@gmail.com>
2016-10-25 19:59:40 -0700

    added things for visual represntation
    probably won’t end up using this though

--- code/scripts/gender-bias-script.R
+++ code/scripts/gender-bias-script.R
@@ -22,9 +22,18 @@ scorecard_male <- temp2[c('dec', 'attr', 'sinc', 'intel', 'fun', 'amb', 'shar')]
 
 ## Visual representation
 
-png('../../images/scorecard-female-boxplot.png')
-boxplot(qualities_1[,-1], horizontal=TRUE, main='Scores of Qualities in a Partner', col='cadetblue2', las=1)
-dev.off()
+### Adding variable names for asthetic
+scorecard_female_with_names <- scorecard_female
+names(scorecard_female_with_names) <- c('Decision', 'Attractive', 'Sincere', 'Fun', 'Ambitious', 'Shared Interests')
+scorecard_male_with_names <- scorecard_male
+names(scorecard_male_with_names) <- c('Decision', 'Attractive', 'Sincere', 'Fun', 'Ambitious', 'Shared Interests')
+
+boxplot(scorecard_female_with_names[,-1], horizontal=TRUE, main='Scores of Qualities in a Partner: Female', col='cadetblue2', las=1)
+
+boxplot(scorecard_male_with_names[,-1], horizontal=TRUE, main='Scores of Qualities in a Partner: Male', col='cadetblue2', las=1)
+
+### Decided to not use this visualization because these were scores they were giving out to other partners
+### not necessarily what they value overall
 
 ################################################
 
